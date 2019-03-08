40ecca7f91c1c865023bba9aa34df9d061e7389c
Vendredimatin <2860957268@qq.com>
2019-03-08 23:04:34 +0800

    complete manager header

--- src/main/resources/static/js/manager/managerStatistics.js
+++ src/main/resources/static/js/manager/managerStatistics.js
@@ -97,4 +97,24 @@ window.onload = function () {
         });
 
     }
+
+    $(".topbar-canteen-area").mouseover(function () {
+        console.log(111);
+        $(".user-menu").css("display", "block");
+    });
+    $(".topbar-canteen-area").mouseout(function () {
+        $(".user-menu").css("display", "none");
+    });
+
+    $(".user-menu").mouseover(function () {
+        $(".user-menu").css("display", "block");
+    })
+
+    $(".user-menu").mouseout(function () {
+        $(".user-menu").css("display", "none");
+    });
+
+    $(".log-out").click(function () {
+        window.location.href = "login.html";
+    })
 };
